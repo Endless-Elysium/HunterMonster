8bf0e884ae69c37f2e6e7121d5c8f13fa21059bb
Ryn <124733337+Endless-Elysium@users.noreply.github.com>
2023-09-01 00:37:48 -0500

    Refactored Player atk

--- HunterMonster/Combat/BattleAction.cs
+++ HunterMonster/Combat/BattleAction.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,21 +13,71 @@ namespace HunterMonster_Combat
     internal class BattleAction
     {
         static Random rng = new Random();
-        private static int PATKDEFCAL(int ATK, int DEF)
+        private static int PATKDEFCAL(int ATK, int WATK, int FATK, int LATK, int DATK, int DEF, int WDEF, int FDEF, int LDEF)
         {
-            if (ATK == 0) return 0;
-            if (ATK - DEF <= 0) return 1;
-            return ATK - DEF;
+            int trueATK = 0;
+
+            if (ATK > 0)
+            {
+                ATK -= DEF;
+                if (ATK < 0) ATK = 1;
+                trueATK += ATK;
+            }
+
+            if (WATK > 0)
+            {
+                WATK -= WDEF;
+                if (WATK < 0) WATK = 1;
+                trueATK += WATK;
+            }
+
+            if (FATK > 0)
+            {
+                FATK -= FDEF;
+                if (FATK < 0) FATK = 1;
+                trueATK += FATK;
+            }
+
+            if (LATK > 0)
+            {
+                LATK -= LDEF;
+                if (LATK < 0) LATK = 1;
+                trueATK += LATK;
+            }
+
+            
+            return trueATK + DATK;
         }
 
-        private static int ATKBONUS(bool a, bool b, bool c, bool d, int damage)
+        private static double ATKBONUS(bool a, bool b, bool c, bool d, int damage)
         {
-            int E = 0;
-            if (a) E += rng.Next(damage + 1);
-            if (b) E += rng.Next(damage + 1);
-            if (c) E += rng.Next(damage + 1);
-            if (d) E += rng.Next(damage + 1);
+            const int CritGain = 5;
+            int E = CritGain;
+            if (a)
+            {
+                E += rng.Next(damage + 1);
+                E += CritGain;
+            }
+            if (b)
+            {
+                E += rng.Next(damage + 1);
+                E += CritGain;
+            }
+            if (c)
+            {
+                E += rng.Next(damage + 1);
+                E += CritGain;
+            }
+            if (d)
+            {
+                E += rng.Next(damage + 1);
+                E += CritGain;
+            }
 
+            if (E > 50)
+            {
+                return 50;
+            }
             return E;
         }
         private static int MATKDEFCAL (int ATK, int DEF)
@@ -36,50 +88,55 @@ namespace HunterMonster_Combat
             return ATK - DEF;
         }
 
-        public static (Hunter, Monster) PlayerAttack(Hunter hunter, Monster m)
+        public static (Hunter, Monster) PlayerAttack(Hunter h, Monster m)
         {
-            bool HeadAlive = m.Head.HP > 0; 
+            bool HeadAlive = m.Head.HP > 0;
             bool ArmAlive = m.Arms.HP > 0;
             bool BodyAlive = m.Body.HP > 0;
             bool TailAlive = m.Tail.HP > 0;
-            int AB = ATKBONUS(!HeadAlive, !ArmAlive, !BodyAlive, !TailAlive, m.RandomDamage);
+            double AB = ATKBONUS(!HeadAlive, !ArmAlive, !BodyAlive, !TailAlive, m.RandomDamage + h.Spe / 2);
 
-            int PlayerAtk = PATKDEFCAL(hunter.PhysDamage + rng.Next(AB + 1), m.Physdef);
+            int PlayerAtk = PATKDEFCAL(h.PhysDamage, h.WaterDamage, h.FireDamage, h.LeafDef, h.DarkDamage, m.Physdef, m.Waterdef, m.Firedef, m.Leafdef);
+            if (h.WeaponType == 2) AB *= 4;
             Console.WriteLine(PlayerAtk);
-            int PlayerWaterAtk = PATKDEFCAL(hunter.WaterDamage, m.Waterdef);
-            int PlayerFireAtk = PATKDEFCAL(hunter.FireDamage, m.Firedef);
-            int PlayerLeafAtk = PATKDEFCAL(hunter.LeafDamage, m.Leafdef);
-
+            PlayerAtk += (int) (PlayerAtk * (AB / 100));
+            Console.WriteLine("AB" + PlayerAtk);
 
             Ui.DrawMonster(m);
             string part = Ui.StringPlayerInput();
 
 
+
             if (HeadAlive && part == "A")
             {
                 Console.WriteLine($"You attacked the {m.Head.Name}");
-                Console.WriteLine($"{m.Name} took {PlayerAtk + PlayerWaterAtk + PlayerFireAtk + PlayerLeafAtk + hunter.DarkDamage}!!");
-                m.AtkHead(PlayerAtk + PlayerWaterAtk + PlayerFireAtk + PlayerLeafAtk + hunter.DarkDamage);
+                Console.WriteLine($"{m.Name} took {PlayerAtk}!!");
+                m.AtkHead(PlayerAtk);
+                if (h.WeaponType == 1) m.AtkHead(Blade2ndATK(PlayerAtk, m.Name));
+
             } else if (ArmAlive && part == "B")
             {
                 Console.WriteLine($"You attacked the {m.Arms.Name}");
-                Console.WriteLine($"{m.Name} took {PlayerAtk + PlayerWaterAtk + PlayerFireAtk + PlayerLeafAtk + hunter.DarkDamage}!!");
-                m.AtkArm(PlayerAtk + PlayerWaterAtk + PlayerFireAtk + PlayerLeafAtk + hunter.DarkDamage);
+                Console.WriteLine($"{m.Name} took {PlayerAtk}!!");
+                m.AtkArm(PlayerAtk);
+                if (h.WeaponType == 1) m.AtkArm(Blade2ndATK(PlayerAtk, m.Name));
             } else if (BodyAlive && part == "C")
             {
                 Console.WriteLine($"You attacked the {m.Body.Name}");
-                Console.WriteLine($"{m.Name} took {PlayerAtk + PlayerWaterAtk + PlayerFireAtk + PlayerLeafAtk + hunter.DarkDamage}!!");
-                m.AtkBody(PlayerAtk + PlayerWaterAtk + PlayerFireAtk + PlayerLeafAtk + hunter.DarkDamage);
+                Console.WriteLine($"{m.Name} took {PlayerAtk}!!");
+                m.AtkBody(PlayerAtk);
+                if (h.WeaponType == 1) m.AtkBody(Blade2ndATK(PlayerAtk, m.Name));
             } else if (TailAlive && part == "D")
             {
                 Console.WriteLine($"You attacked the {m.Tail.Name}");
-                Console.WriteLine($"{m.Name} took {PlayerAtk + PlayerWaterAtk + PlayerFireAtk + PlayerLeafAtk + hunter.DarkDamage}!!");
-                m.AtkTail(PlayerAtk + PlayerWaterAtk + PlayerFireAtk + PlayerLeafAtk + hunter.DarkDamage);
+                Console.WriteLine($"{m.Name} took {PlayerAtk}!!");
+                m.AtkTail(PlayerAtk);
+                if (h.WeaponType == 1) m.AtkTail(Blade2ndATK(PlayerAtk, m.Name));
             } else
             {
                 Console.WriteLine($"You attacked the Monster!");
-                Console.WriteLine($"{m.Name} took {PlayerAtk + PlayerWaterAtk + PlayerFireAtk + PlayerLeafAtk + hunter.DarkDamage}!!");
-                m.HP -= PlayerAtk + PlayerWaterAtk + PlayerFireAtk + PlayerLeafAtk + hunter.DarkDamage;
+                Console.WriteLine($"{m.Name} took {PlayerAtk}!!");
+                m.HP -= PlayerAtk;
             }
 
             if (m.Head.HP <= 0 && m.Arms.HP <= 0 && m.Body.HP <= 0 && m.Tail.HP <= 0)
@@ -87,8 +144,28 @@ namespace HunterMonster_Combat
                 m.HP = 0;
             }
 
-            return (hunter, m);
+            return (h, m);
         }
+
+
+        private static int Blade2ndATK(int ATK, string m)
+        {
+            if (rng.Next(6) != 5) return 0;
+
+
+            Console.WriteLine($"Your swift attacks allowed you to strike once more!");
+            Console.WriteLine($"{m} took {ATK}!!");
+
+            return ATK;
+
+        }
+
+
+
+
+
+
+
 
         public static int PhysMonsterATK(Monster m, Hunter h)
         {
